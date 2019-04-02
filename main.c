af9bc67ea1e311adc4643b0575a07adef81328c5
Tim O'Connell <c3184147@uon.edu.au>
2019-04-02 02:25:40 +0000

    master: Main menu

--- main.c
+++ main.c
@@ -1,11 +1,32 @@
 //hellowolrd
 #include <stdio.h>
 int main(){
+    // Made a menu interface which comes up with the first 4 options for the project
+    printf("Please select an option\n");
+    printf("a) Encryption with a rotation cipher key:\n");
+    printf("b) Decryption with a rotation cipher key:\n");
+    printf("c) Encryption with a substitution cipher given plain text and key:\n");
+    printf("d) Decryption with a substitution cipher given plain text and key:\n");
     
     
     
-    printf("Hello world\n");
+    char d[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // An array for the alphabet using a string
+    //the string was started by using the ""
     
+    printf("%c\n", d[6]); // the letter you want minus 1, 
+    //so G is the 7th letter but you'd write 6
+    
+    printf("%s\n", d);
+    // the %s lets you type a string 
+    
+    scanf("%s", d);
+    //the %s scans the string
+    
+     printf("%s\n", d);
+    char c;
+   // scanf("%c", c);
+    //c = c - 65;
+   
     
     
     
