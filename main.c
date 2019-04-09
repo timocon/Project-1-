a7f7c6e23bf366eb429651b205408a5c0852f655
Tim O'Connell <c3184147@uon.edu.au>
2019-04-09 03:21:53 +0000

    master: engg1003

--- main.c
+++ main.c
@@ -1,43 +1,48 @@
 //hellowolrd
 #include <stdio.h>
+
+
 int main(){
     
     
     
     // Made a menu interface which comes up with the first 4 options for the project
-    printf("Please select an option\n");
-    printf("a) Encryption with a rotation cipher key:\n");
-    printf("b) Decryption with a rotation cipher key:\n");
-    printf("c) Encryption with a substitution cipher given plain text and key:\n");
-    printf("d) Decryption with a substitution cipher given plain text and key:\n");
-    char a;
-    scanf("%c", &a);
-    switch (a) {
-        case 'a': encryptrotationkey(); break;
-        case 'b': decryptrotationkey(); break;
-        case 'c': encryptsubstitutionkey(); break;
-        case 'd': decryptsubstitutionkey(); break;
-        default; printf("unknown option %a\n Please enter a, b, c or d\n", a);
+   // printf("Please select an option\n");
+    //printf("a) Encryption with a rotation cipher key:\n");
+    //printf("b) Decryption with a rotation cipher key:\n");
+    //printf("c) Encryption with a substitution cipher given plain text and key:\n");
+    //printf("d) Decryption with a substitution cipher given plain text and key:\n");
+    //char a;
+    //scanf("%c", &a);
+    //switch (a) {
+      //  case 'a': encryptrotationkey(); break;
+        //case 'b': decryptrotationkey(); break;
+        //case 'c': encryptsubstitutionkey(); break;
+        //case 'd': decryptsubstitutionkey(); break;
+        //default; printf("unknown option %a\n Please enter a, b, c or d\n", a);
         //a switch statement for the user interface section. dont know if i still need the top section
         // with the printf's 
-    }
+    
     // need to make a user input section, week 6 lecture has stuff on it using a switch with letters  
     
     
     //char d[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // An array for the alphabet using a string
     //the string was started by using the ""
-    char c[100] ; 
+    char c[100] = "hello how are you"; 
     //example letter
     int key = 2;
     
+    //sizeof(c)/sizeof(char); 
+   //'\0' 
     printf("Enter a letter to encrypt: ");
-    scanf (" %s", &c); // scans the letter entered and adds it to char c 
-   
+    scanf (" %[^\n]s", c); // scans the letter entered and adds it to char c 
+   // [^\n] ignores the space
     int counter = 0;
-    for (counter = 0; counter <25; counter++)
+    for (counter = 0; counter<10; counter++)
+    //for (counter = 0; counter<c[counter]; counter++)
     
     {
-       
+       if(c[counter]!=32) //allows you to use space bar
         c[counter] = c[counter] + key;
     }
     
