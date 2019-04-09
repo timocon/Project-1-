460711db9cb4d35923debce016b735d0c4a4e9b1
Tim O'Connell <c3184147@uon.edu.au>
2019-04-09 02:37:41 +0000

    master: engg1003

--- main.c
+++ main.c
@@ -13,33 +13,34 @@ int main(){
     // need to make a user input section, week 6 lecture has stuff on it using a switch with letters  
     
     
-    char d[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // An array for the alphabet using a string
+    //char d[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // An array for the alphabet using a string
     //the string was started by using the ""
-    char c = 'a'; 
+    char c[100] ; 
     //example letter
     int key = 2;
     
+    //printf("Enter a letter to encrypt: ");
+    //scanf (" %s", c); // scans the letter entered and adds it to char c 
+   
     int counter = 0;
     for (counter = 0; counter <25; counter++)
     
     {
-        d[counter] = d[counter] + key;
+        printf("enter a letter:");
+        scanf("%s", &c);
+        
+        c[counter] = c[counter] + key;
     }
     
-    printf("%s\n", d);
+    printf("%s\n", c);
     // the %s lets you type a string, should be the array + the key numeral 
     
     
     //printf("%c\n", d[0]); // the letter you want minus 1, 
-    //so G is the 7th letter but you'd write 6
-    
-     
-    
-    //scanf("%s", d);
+    //so G is the 7th letter but you'd write 6 
+      //scanf("%s", d);
     //the %s scans the string
-    
      //printf("%s\n", d);
-    
    // scanf("%c", c);
     //c = c - 65;
    // should make the letter c worth the numerical value 3
