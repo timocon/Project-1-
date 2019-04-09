05747239d1653ea8d806126ed4f57c81f9a716c0
Tim O'Connell <c3184147@uon.edu.au>
2019-04-09 02:07:28 +0000

    made the key for the rotation cipher

--- main.c
+++ main.c
@@ -2,32 +2,46 @@
 #include <stdio.h>
 int main(){
     
-    char c;
+    
+    
     // Made a menu interface which comes up with the first 4 options for the project
     printf("Please select an option\n");
     printf("a) Encryption with a rotation cipher key:\n");
     printf("b) Decryption with a rotation cipher key:\n");
     printf("c) Encryption with a substitution cipher given plain text and key:\n");
     printf("d) Decryption with a substitution cipher given plain text and key:\n");
-    // need to make a user input section, week 6 lecture has stuff on it  
+    // need to make a user input section, week 6 lecture has stuff on it using a switch with letters  
     
     
     char d[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // An array for the alphabet using a string
     //the string was started by using the ""
+    char c = 'a'; 
+    //example letter
+    int key = 2;
     
-    printf("%c\n", d[0]); // the letter you want minus 1, 
-    //so G is the 7th letter but you'd write 6
+    int counter = 0;
+    for (counter = 0; counter <25; counter++)
+    
+    {
+        d[counter] = d[counter] + key;
+    }
     
     printf("%s\n", d);
-    // the %s lets you type a string 
+    // the %s lets you type a string, should be the array + the key numeral 
     
-    scanf("%s", d);
+    
+    //printf("%c\n", d[0]); // the letter you want minus 1, 
+    //so G is the 7th letter but you'd write 6
+    
+     
+    
+    //scanf("%s", d);
     //the %s scans the string
     
-     printf("%s\n", d);
+     //printf("%s\n", d);
     
    // scanf("%c", c);
-    c = c - 65;
+    //c = c - 65;
    // should make the letter c worth the numerical value 3
     // i think im meant to use this to make the rotation cipher key
     
