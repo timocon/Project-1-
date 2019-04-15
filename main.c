0143898299b1977f02919113dd301ae3137bf6ad
Tim O'Connell <c3184147@uon.edu.au>
2019-04-15 02:28:45 +0000

    master: engg1003

--- main.c
+++ main.c
@@ -17,12 +17,15 @@ int main(){
     //printf("d) Decryption with a substitution cipher given plain text and key:\n");
     //char a;
     //scanf("%c", &a);
+    
     //switch (a) {
       //  case 'a': encryptrotationkey(); break;
         //case 'b': decryptrotationkey(); break;
         //case 'c': encryptsubkey(); break;
         //case 'd': decryptsubkey(); break;
-        //default; printf("unknown option %a\n Please enter a, b, c or d\n", a);
+        //default: printf("unknown option %d\n Please enter a, b, c or d\n", a);
+    //}
+
         //a switch statement for the user interface section. dont know if i still need the top section
         // with the printf's 
     
