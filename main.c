6b2c26aea2338f0f0fd89c8d075e812cae32239a
Tim O'Connell <c3184147@uon.edu.au>
2019-04-28 12:56:26 +0000

    master: engg1003

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
-char encryptrotationkey (char, int); //caesar cipher encryption function
-char decryptrotationkey (char, int); //caesar cipher decryption function
+char encryptrotationkey (char[], int); //caesar cipher encryption function
+char decryptrotationkey (char[], int); //caesar cipher decryption function
 char encryption (char, char);// substitution cipher encryption function
 char decryption (char, char);// substitution cipher decryption function
 
@@ -16,43 +16,37 @@ int main(){
     printf("d) Decryption with a substitution cipher given plain text and key:\n");
     char a;
     scanf("%c", &a);
-    
+    char var1;  //acts as the encrpyted/decrypted char in all cases
+    int var2; //acts as the key for the switch cases
+    char var3;
     while(a <'a' || a > 'd') // make the menu input only read the letters a,b,c,d 
      
     switch (a) 
         {
         case 'a': 
                     printf("Enter a word:\n");
-                    char 1;
-                    scanf("%c", &1);
+                    scanf("%c", &var1);
                     printf("Enter your key:\n");
-                    int 2;
-                    scanf("%d", &2);
-        encryptrotationkey(1,2); break;
+                    scanf("%d", &var2);
+        encryptrotationkey(var1,var2); break;
         case 'b':
-                    printf("Enter a word :\n");
-                    char 1;
-                    scanf("%c", &1);
+                    printf("Enter a word:\n");
+                    scanf("%c", &var1);
                     printf("Enter your key:\n");
-                    int 2;
-                    scanf("%d", &2);
-        decryptrotationkey(1,2); break;
+                    scanf("%d", &var2);
+        decryptrotationkey(var1,var2); break;
         case 'c': 
-                    printf("Enter a word :\n");
-                    char 1;
-                    scanf("%c", &1);
+                    printf("Enter a word:\n");
+                    scanf("%c", &var1);
                     printf("Enter your key:\n");
-                    char 2;
-                    scanf("%d", &2);
-        encryption(1,2); break;
+                    scanf("%", &var3);
+        encryption(var1,var2); break;
         case 'd': 
-                    printf("Enter a word :\n");
-                    char 1;
-                    scanf("%c", &1);
+                    printf("Enter a word:\n");
+                    scanf("%c", &var1);
                     printf("Enter your key:\n");
-                    char 2;
-                    scanf("%d", &2);
-        decryption(1,2); break;
+                    scanf("%d", &var3);
+        decryption(var1,var2); break;
         default: printf("unknown option %d\n Please enter a, b, c or d\n", a);
         }
         
@@ -63,10 +57,16 @@ int main(){
  ///////////////////////////////////////////////
  //////////////////////////////////////////////
  
-    char encryptrotationkey (char c, int key)
+   // #include <stdio.h>
+   // #include <string.h>
+    char encryptrotationkey (char c[100], int key)
+    //int main()
+    //switch statements arent working, leaving int main so i can copy and paste to show 
+    // that individual code works, also leaving char, and int commented out for same reason 
     {
-    char c[100]; //array number equals how many letters can be encrypted
-    int key; //key is how many letters it needs to move to the right 
+        // char c[100]; //array number equals how many letters can be encrypted
+        // int key; //key is how many letters it needs to move to the right 
+        //remove comments from the char and int to run program individually
     printf("Enter a letter to encrypt (Caps only): ");
     scanf (" %[^\n]s", c); // scans the letter entered and adds it to char c 
     // [^\n] ignores the space
@@ -82,26 +82,34 @@ int main(){
   
     printf("%s\n", c);
     return encryptrotationkey;
+    //return c;  //if running individually
     }   
 ////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////
+
+
 //#include<stdio.h>
+//#include <string.h>
 
 //decipher code: looks like this works
    
-   char decryptrotationkey (char, int);
-  // int main()
+   char decryptrotationkey (char [100], int key);
+  // int main() 
+  //switch statements arent working, leaving int main so i can copy and paste to show 
+    // that individual code works, also leaving char, and int commented out for same reason 
    {
-       char c[100]; //array number equals how many letters can be decrypted
-       int key ; 
-       key = 26 - key; //cant figure out how to make a go to z 
-       // but using 26 minus the key does it by rotating to the right 
-       //using the total letters in the alphabet - the key 
+       //char c[100]; //array number equals how many letters can be decrypted
+       //int key ; 
+      
        int counter = 0;
        printf("Enter a letter to encrypt (Caps only): ");
        scanf (" %[^\n]s", c);
        printf("Enter a number key:");
        scanf("%d", &key);
+        key = 26 - key; //cant figure out how to make a go to z 
+       // but using 26 minus the key does it by rotating to the right 
+       //using the total letters in the alphabet - the key 
+       int counter = 0;
        for (counter = 0; counter<50; counter++) // change the '<' number for how much output you want
        if(c[counter]!=32)// makes the key ignore ' ' (blank space/space bar)
        if(c[counter]!=58)// makes the key ignore ':' 
@@ -112,8 +120,8 @@ int main(){
        //this way i can make the key work properly and get a to turn to z 
        
        printf("%s\n", c);
-       return c; //not sure if this is right?
-   }
+       return decryptrotationkey;
+       //return c; //use if running individually
    
   
 
@@ -122,16 +130,19 @@ int main(){
  /////////////////////////////////////////////////////
  ///the switch statements have some errors that dont let the substitution ciphers run
  // so i rewrote them to work individually 
+  
+  //switch statements arent working, leaving int main so i can copy and paste to show 
+    // that individual code works, also leaving char, and int commented out for same reason
  
 //#include <stdio.h>
 //#include <string.h>
 
-char encryption (char cipher_text, char key)
+char encryption (char cipher_text[], char key[26])
 
 char alpha[26] ={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-char key [26];
-char cipher_text[];
-int main ()
+//char key [26];
+//char cipher_text[];
+//int main ()
 {
     
     char *c_text, msg[255];
@@ -171,15 +182,18 @@ int main ()
       //subsitution decryption program works on its own 
       //#include <stdio.h>
       //#include <string.h>
+       // int main() 
+  //switch statements arent working, leaving int main so i can copy and paste to show 
+    // that individual code works, also leaving char, and int commented out for same reason
 
 //sub decryption
 
-char decryption (char cipher_text, char key)
+char decryption (char cipher_text[], char key[26])
 
 char alpha[26] ={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-char key [26];
-char cipher_text[];
-int main ()
+//char key [26];
+//char cipher_text[];
+//int main ()
 {
     
       char *c_text, msg[255];
